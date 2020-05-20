cdbb28c0d2b44d18f819f3f26776e539f80fcbf5
david sastre <davidsastresas@gmail.com>
2020-05-20 20:58:28 +0200

    srcground: move ground code to srcground

---
+++ srcground/Config.h
@@ -0,0 +1,297 @@
+/*
+ Config.h
+
+ This file is a part of RadioRoom project.
+
+ (C) Copyright 2017-2019 Envirover.
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#ifndef SRC_CONFIG_H_
+#define SRC_CONFIG_H_
+
+#include <string>
+
+namespace radioroom {
+
+constexpr char default_config_file[] = "/etc/radioroomground.conf";
+
+/**
+ * RadioRoom configuration properties.
+ */
+class Config {
+ public:
+  Config();
+
+  /*
+   * Loads configuration from the specified config file.
+   *
+   * Returns 0 in case of success and exit code in case of invalid
+   * configuration.
+   */
+  int init(const std::string& config_file);
+
+  /* Autopilot configuration properties */
+
+  std::string get_autopilot_serial() const;
+  void set_autopilot_serial(const std::string& path);
+
+  int get_autopilot_serial_speed() const;
+  void set_autopilot_serial_speed(int speed);
+
+  bool get_autopilot_enabled() const;
+  void set_autopilot_enabled(bool enabled);
+
+  /* rfd configuration properties */
+
+  std::string get_rfd_serial() const;
+  void set_rfd_serial(const std::string& path);
+
+  int get_rfd_serial_speed() const;
+  void set_rfd_serial_speed(int speed);
+
+  bool get_rfd_enabled() const;
+  void set_rfd_enabled(bool enabled);
+
+  /* RadioRoom configuration properties */
+
+  bool get_auto_detect_serials() const;
+  void set_auto_detect_serials(bool a);
+  
+  bool get_air_unit() const;
+  void set_air_unit(bool a);
+
+  bool get_debug_mode() const;
+  void set_debug_mode(bool debug);
+
+  /* ISBD comm link configuration properties */
+
+  bool get_isbd_enabled() const;
+  void set_isbd_enabled(bool enabled);
+
+  std::string get_isbd_serial() const;
+  void set_isbd_serial(const std::string& path);
+
+  int get_isbd_serial_speed() const;
+  void set_isbd_serial_speed(int speed);
+
+  double get_isbd_report_period() const;
+  void set_isbd_report_period(double period);
+
+  /* TCP/IP comm link configuration properties */
+
+  bool get_tcp_enabled() const;
+  void set_tcp_enabled(bool enabled);
+
+  std::string get_tcp_host() const;
+  void set_tcp_host(const std::string& host);
+
+  int get_tcp_port() const;
+  void set_tcp_port(int port);
+
+  double get_tcp_report_period() const;
+  void set_tcp_report_period(double period);
+
+  /* groundstation configuration properties */
+
+  std::string get_groundstation_tlf_number1() const;
+  void set_groundstation_tlf_number1(const std::string& number);
+
+  std::string get_groundstation_tlf_number2() const;
+  void set_groundstation_tlf_number2(const std::string& number);
+  
+  std::string get_groundstation_tlf_number3() const;
+  void set_groundstation_tlf_number3(const std::string& number);
+
+  std::string get_groundstation_rock_address() const;
+  void set_groundstation_rock_address(const std::string& address);
+
+  int get_groundstation_mav_id() const;
+  void set_groundstation_mav_id(int mavid);
+
+  /* aircraft1 configuration properties */
+
+  bool get_aircraft1_enabled() const;
+  void set_aircraft1_enabled(bool enabled);
+
+  std::string get_aircraft1_tlf_number1() const;
+  void set_aircraft1_tlf_number1(const std::string& number);
+  
+  std::string get_aircraft1_tlf_number2() const;
+  void set_aircraft1_tlf_number2(const std::string& number);
+  
+  std::string get_aircraft1_tlf_number3() const;
+  void set_aircraft1_tlf_number3(const std::string& number);
+
+  std::string get_aircraft1_rock_address() const;
+  void set_aircraft1_rock_address(const std::string& address);
+
+  int get_aircraft1_mav_id() const;
+  void set_aircraft1_mav_id(int mavid);
+
+  /* aircraft2 configuration properties */
+
+  bool get_aircraft2_enabled() const;
+  void set_aircraft2_enabled(bool enabled);
+
+  std::string get_aircraft2_tlf_number1() const;
+  void set_aircraft2_tlf_number1(const std::string& number);
+
+  std::string get_aircraft2_tlf_number2() const;
+  void set_aircraft2_tlf_number2(const std::string& number);
+  
+  std::string get_aircraft2_tlf_number3() const;
+  void set_aircraft2_tlf_number3(const std::string& number);
+
+  std::string get_aircraft2_rock_address() const;
+  void set_aircraft2_rock_address(const std::string& address);
+
+  int get_aircraft2_mav_id() const;
+  void set_aircraft2_mav_id(int mavid);
+
+  /* aircraft3 configuration properties */
+
+  bool get_aircraft3_enabled() const;
+  void set_aircraft3_enabled(bool enabled);
+
+  std::string get_aircraft3_tlf_number1() const;
+  void set_aircraft3_tlf_number1(const std::string& number);
+  
+  std::string get_aircraft3_tlf_number2() const;
+  void set_aircraft3_tlf_number2(const std::string& number);
+
+  std::string get_aircraft3_tlf_number3() const;
+  void set_aircraft3_tlf_number3(const std::string& number);
+
+  std::string get_aircraft3_rock_address() const;
+  void set_aircraft3_rock_address(const std::string& address);
+
+  int get_aircraft3_mav_id() const;
+  void set_aircraft3_mav_id(int mavid);
+
+  /* aircraft4 configuration properties */
+
+  bool get_aircraft4_enabled() const;
+  void set_aircraft4_enabled(bool enabled);
+
+  std::string get_aircraft4_tlf_number1() const;
+  void set_aircraft4_tlf_number1(const std::string& number);
+  
+  std::string get_aircraft4_tlf_number2() const;
+  void set_aircraft4_tlf_number2(const std::string& number);
+
+  std::string get_aircraft4_tlf_number3() const;
+  void set_aircraft4_tlf_number3(const std::string& number);
+
+  std::string get_aircraft4_rock_address() const;
+  void set_aircraft4_rock_address(const std::string& address);
+
+  int get_aircraft4_mav_id() const;
+  void set_aircraft4_mav_id(int mavid);
+
+  /* aircraft5 configuration properties */
+
+  bool get_aircraft5_enabled() const;
+  void set_aircraft5_enabled(bool enabled);
+
+  std::string get_aircraft5_tlf_number1() const;
+  void set_aircraft5_tlf_number1(const std::string& number);
+
+  std::string get_aircraft5_tlf_number2() const;
+  void set_aircraft5_tlf_number2(const std::string& number);
+  
+  std::string get_aircraft5_tlf_number3() const;
+  void set_aircraft5_tlf_number3(const std::string& number);
+
+  std::string get_aircraft5_rock_address() const;
+  void set_aircraft5_rock_address(const std::string& address);
+
+  int get_aircraft5_mav_id() const;
+  void set_aircraft5_mav_id(int mavid);
+
+
+
+ private:
+
+  bool autopilot_enabled;
+  std::string autopilot_serial;
+  int autopilot_serial_speed;
+  
+  bool rfd_enabled;
+  std::string rfd_serial;
+  int rfd_serial_speed;
+
+  bool auto_detect_serials;
+  bool air_unit;
+  bool debug_mode;
+
+  bool isbd_enabled;
+  std::string isbd_serial;
+  int isbd_serial_speed;
+  double isbd_report_period;
+
+  bool tcp_enabled;
+  std::string tcp_host;
+  int tcp_port;
+  double tcp_report_period;
+
+  bool groundstation_enabled;
+  std::string groundstation_tlf_number1;
+  std::string groundstation_tlf_number2;
+  std::string groundstation_tlf_number3;
+  std::string groundstation_rock_address;
+  int groundstation_mav_id;
+  
+  bool aircraft1_enabled;
+  std::string aircraft1_tlf_number1;
+  std::string aircraft1_tlf_number2;
+  std::string aircraft1_tlf_number3;
+  std::string aircraft1_rock_address;
+  int aircraft1_mav_id;
+
+  bool aircraft2_enabled;
+  std::string aircraft2_tlf_number1;
+  std::string aircraft2_tlf_number2;
+  std::string aircraft2_tlf_number3;
+  std::string aircraft2_rock_address;
+  int aircraft2_mav_id;
+
+  bool aircraft3_enabled;
+  std::string aircraft3_tlf_number1;
+  std::string aircraft3_tlf_number2;
+  std::string aircraft3_tlf_number3;
+  std::string aircraft3_rock_address;
+  int aircraft3_mav_id;
+
+  bool aircraft4_enabled;
+  std::string aircraft4_tlf_number1;
+  std::string aircraft4_tlf_number2;
+  std::string aircraft4_tlf_number3;
+  std::string aircraft4_rock_address;
+  int aircraft4_mav_id;
+
+  bool aircraft5_enabled;
+  std::string aircraft5_tlf_number1;
+  std::string aircraft5_tlf_number2;
+  std::string aircraft5_tlf_number3;
+  std::string aircraft5_rock_address;
+  int aircraft5_mav_id;
+};
+
+extern Config config;
+
+}  // namespace radioroom
+
+#endif  // SRC_CONFIG_H_
---
+++ srcground/radioroom.cc
@@ -0,0 +1,146 @@
+/*
+ radioroom.cpp
+
+ Telemetry for MAVLink autopilots.
+
+ (C) Copyright 2018-2019 Envirover.
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <atomic>
+#include <iostream>
+
+#include "Config.h"
+#include "MAVLinkHandlerGround.h"
+#include "MAVLinkLogger.h"
+#include "build.h"
+#include "timelib.h"
+
+using mavio::log;
+using radioroom::MAVLinkHandlerGround;
+using std::cout;
+using std::endl;
+
+constexpr char log_identity[] = "radioroom";
+
+const std::chrono::milliseconds msg_handler_loop_period(1);
+
+std::atomic<bool> running(false);
+
+void print_help() {
+  cout << "Usage: radioroom [options]" << endl;
+  cout << "options:" << endl;
+  cout << "    -c <config file>" << endl;
+  cout << "          Alternative configuration file instead of "
+          "/etc/radioroom.conf."
+       << endl;
+  cout << endl;
+  cout << "    -h    Print this help and exit." << endl;
+  cout << endl;
+  cout << "    -v    Verbose logging." << endl;
+  cout << endl;
+  cout << "    -V    Print version and exit." << endl;
+}
+
+void print_version() {
+  cout << RADIO_ROOM_VERSION << "." << BUILD_NUM << endl;
+  cout << "MAVLink wire protocol version " << MAVLINK_WIRE_PROTOCOL_VERSION
+       << endl;
+}
+
+void handle_signal(int sig) {
+  if (sig == SIGTERM) {
+    running = false;
+
+    /* Reset signal handling to default behavior */
+    signal(SIGTERM, SIG_DFL);
+  }
+}
+
+int main(int argc, char** argv) {
+  MAVLinkHandlerGround msg_handler;
+  std::string config_file = radioroom::default_config_file;
+
+  int c;
+  while ((c = getopt(argc, argv, "c:hvV")) != -1) {
+    switch (c) {
+      case 'c':
+        config_file = optarg;
+        break;
+      case 'h':
+        print_help();
+        return EXIT_SUCCESS;
+      case 'v':
+        radioroom::config.set_debug_mode(true);
+        break;
+      case 'V':
+        print_version();
+        return EXIT_SUCCESS;
+      case '?':
+        if (optopt == 'c') {
+          cout << "Option -c requires an argument." << endl;
+        } else if (isprint(optopt)) {
+          cout << "Unknown option '-" << std::string(1, optopt) << "'." << endl;
+        } else {
+          cout << "Unknown option character '" << std::string(1, optopt) << "'."
+               << endl;
+        }
+        return EXIT_FAILURE;
+    }
+  }
+
+  mavio::openlog(log_identity, radioroom::config.get_debug_mode()
+                                   ? LOG_UPTO(LOG_DEBUG)
+                                   : LOG_UPTO(LOG_INFO));
+
+  log(LOG_INFO, "Starting %s.%s...", RADIO_ROOM_VERSION, BUILD_NUM);
+
+  if (radioroom::config.init(config_file) < 0) {
+    log(LOG_ERR, "Can't load configuration file '%s'", config_file.data());
+  }
+
+  if (msg_handler.init()) {
+    log(LOG_NOTICE, "%s.%s started.", RADIO_ROOM_VERSION, BUILD_NUM);
+  } else {
+    log(LOG_CRIT, "%s.%s initialization failed.", RADIO_ROOM_VERSION,
+        BUILD_NUM);
+    return EXIT_FAILURE;
+  }
+
+  signal(SIGTERM, handle_signal);
+
+  running = true;
+
+  while (running) {
+    if (msg_handler.loop()) {
+      timelib::sleep(msg_handler_loop_period);
+    }
+  }
+
+  log(LOG_INFO, "Stopping %s.%s...", RADIO_ROOM_VERSION, BUILD_NUM);
+
+  msg_handler.close();
+
+  log(LOG_NOTICE, "%s.%s stopped.", RADIO_ROOM_VERSION, BUILD_NUM);
+
+  mavio::closelog();
+
+  return EXIT_SUCCESS;
+}
