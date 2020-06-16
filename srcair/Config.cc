9a2b06cd4cd78ab3cefc099520fad35e6dfed0b8
david sastre <davidsastresas@gmail.com>
2020-06-16 03:27:02 +0200

    scrair/config: Complete redo

--- srcair/Config.cc
+++ srcair/Config.cc
@@ -27,62 +27,41 @@ namespace radioroom {
 
 Config config;
 
-constexpr bool default_autopilot_enabled = false;
-constexpr char default_autopilot_serial[] = "/dev/ttyusb0";
-constexpr int autopilot_serial_baud_rate = 115200;
-
-constexpr bool default_rfd_enabled = false;
-constexpr char default_rfd_serial[] = "/dev/ttyusb1";
-constexpr int rfd_serial_baud_rate = 115200;
-
-constexpr bool default_isbd_enabled = false;
-constexpr char default_isbd_serial[] = "/dev/ttyusb1";
-constexpr int isbd_serial_baud_rate = 19200;
-
-constexpr bool default_tcp_enabled = false;
-constexpr char default_tcp_host[] = "";
-constexpr int default_tcp_port = 5060;
-
-constexpr double default_isbd_report_period = 300.0;  // 5 minutes
-constexpr double default_tcp_report_period = 60.0;    // 1 minute
-
 constexpr bool default_enabled = false;
 constexpr int default_int = 0;
 constexpr char default_string[] = "0";
 
 // radioroom.conf properties
-constexpr char autopilot_enabled_property[] = "enabled";
 constexpr char autopilot_config_section[] = "autopilot";
 constexpr char autopilot_serial_property[] = "serial";
 constexpr char autopilot_serial_speed_property[] = "serial_speed";
 
-constexpr char rfd_enabled_property[] = "enabled";
 constexpr char rfd_config_section[] = "rfd";
 constexpr char rfd_serial_property[] = "serial";
 constexpr char rfd_serial_speed_property[] = "serial_speed";
 
 constexpr char radioroom_config_section[] = "radioroom";
-constexpr char auto_detect_serials_property[] = "auto_detect_serials";
-constexpr char air_unit_property[] = "air_unit";
-constexpr char report_period_property[] = "report_period";
 
 constexpr char isbd_config_section[] = "isbd";
-constexpr char isbd_enabled_property[] = "enabled";
 constexpr char isbd_serial_property[] = "serial";
 constexpr char isbd_serial_speed_property[] = "serial_speed";
 
+constexpr char gsm_config_section[] = "gsm";
+constexpr char gsm_serial_property[] = "serial";
+constexpr char gsm_serial_speed_property[] = "serial_speed";
+
 constexpr char tcp_config_section[] = "tcp";
-constexpr char tcp_enabled_property[] = "enabled";
-constexpr char tcp_host_property[] = "host";
 constexpr char tcp_port_property[] = "port";
 
+constexpr char misc_config_section[] = "misc";
+constexpr char debug_mode_property[] = "debug";
+
+constexpr char groundstation_config_section[] = "groundstation";
 constexpr char aircraft1_config_section[] = "aircraft1";
 constexpr char aircraft2_config_section[] = "aircraft2";
 constexpr char aircraft3_config_section[] = "aircraft3";
 constexpr char aircraft4_config_section[] = "aircraft4";
 constexpr char aircraft5_config_section[] = "aircraft5";
-
-constexpr char groundstation_config_section[] = "groundstation";
 
 constexpr char enabled[] = "enabled";
 constexpr char tlf_number1[] = "tlf_number1";
@@ -92,19 +71,7 @@ constexpr char rock_address[] = "rock_address";
 constexpr char mav_id[] = "mav_id";
 
 // TODO we may need to check this out
-Config::Config()
-    : autopilot_serial(default_autopilot_serial),
-      autopilot_serial_speed(autopilot_serial_baud_rate),
-      auto_detect_serials(true),
-      debug_mode(false),
-      isbd_enabled(default_isbd_enabled),
-      isbd_serial(default_isbd_serial),
-      isbd_serial_speed(isbd_serial_baud_rate),
-      isbd_report_period(default_isbd_report_period),
-      tcp_enabled(default_tcp_enabled),
-      tcp_host(default_tcp_host),
-      tcp_port(default_tcp_port),
-      tcp_report_period(default_tcp_report_period) {}
+Config::Config() {}
 
 int Config::init(const std::string& config_file) {
   INIReader conf(config_file);
@@ -117,67 +84,44 @@ int Config::init(const std::string& config_file) {
 
   /* [autopilot] config section */
 
-  set_autopilot_enabled(conf.GetBoolean(autopilot_config_section, autopilot_enabled_property,
-                                   default_autopilot_enabled));
-
-
   set_autopilot_serial(conf.Get(autopilot_config_section,
-                                autopilot_serial_property,
-                                default_autopilot_serial));
+                       autopilot_serial_property, default_string));
 
   set_autopilot_serial_speed(conf.GetInteger(autopilot_config_section,
-                                             autopilot_serial_speed_property,
-                                             autopilot_serial_baud_rate));
+                             autopilot_serial_speed_property, default_int));
 
   /* [rfd] config section */
 
-  set_rfd_enabled(conf.GetBoolean(rfd_config_section, rfd_enabled_property,
-                                   default_rfd_enabled));
-
   set_rfd_serial(conf.Get(rfd_config_section,
-                                rfd_serial_property,
-                                default_rfd_serial));
+                  rfd_serial_property, default_string));
 
   set_rfd_serial_speed(conf.GetInteger(rfd_config_section,
-                                             rfd_serial_speed_property,
-                                             rfd_serial_baud_rate));
-
-  /* [radioroom] config section */
-
-  set_auto_detect_serials(conf.GetBoolean(radioroom_config_section,
-                                          auto_detect_serials_property, true));
-
-  set_air_unit(conf.GetBoolean(radioroom_config_section,
-                                          air_unit_property, true));
+                       rfd_serial_speed_property, default_int));
 
   /* [isbd] config section */
 
-  set_isbd_enabled(conf.GetBoolean(isbd_config_section, isbd_enabled_property,
-                                   default_isbd_enabled));
+  set_isbd_serial(conf.Get(isbd_config_section, isbd_serial_property,
+                  default_string));
 
-  set_isbd_serial(
-      conf.Get(isbd_config_section, isbd_serial_property, default_isbd_serial));
+  set_isbd_serial_speed(conf.GetInteger(isbd_config_section, 
+                        isbd_serial_speed_property, default_int));
 
-  set_isbd_serial_speed(conf.GetInteger(
-      isbd_config_section, isbd_serial_speed_property, isbd_serial_baud_rate));
+  /* [gsm] config section */
 
-  set_isbd_report_period(conf.GetReal(
-      isbd_config_section, report_period_property, default_isbd_report_period));
+  set_gsm_serial(conf.Get(gsm_config_section, gsm_serial_property, default_string));
+
+  set_gsm_serial_speed(conf.GetInteger(
+      gsm_config_section, gsm_serial_speed_property, default_int));
 
   /* [tcp] config section */
 
-  set_tcp_enabled(conf.GetBoolean(tcp_config_section, tcp_enabled_property,
-                                  default_tcp_enabled));
-
-  set_tcp_host(
-      conf.Get(tcp_config_section, tcp_host_property, default_tcp_host));
-
   set_tcp_port(
-      conf.GetInteger(tcp_config_section, tcp_port_property, default_tcp_port));
+      conf.GetInteger(tcp_config_section, tcp_port_property, default_int));
 
-  set_tcp_report_period(conf.GetReal(tcp_config_section, report_period_property,
-                                     default_tcp_report_period));
+  /* [misc] config section */
 
+  set_debug_mode(conf.GetBoolean(misc_config_section, debug_mode_property, default_enabled));
+  
   /* [groundstation] config section */ 
 
   set_groundstation_tlf_number1(conf.Get(groundstation_config_section,
@@ -295,114 +239,74 @@ int Config::init(const std::string& config_file) {
   set_aircraft4_mav_id(conf.GetInteger(
         aircraft4_config_section, mav_id, default_int));
   
-    /* [aircraft5] config section */ 
-
-    set_aircraft5_enabled(conf.GetBoolean(aircraft5_config_section, enabled,
-                                    default_enabled));
-
-    set_aircraft5_tlf_number1(conf.Get(aircraft5_config_section,
-                                  tlf_number1,
-                                  default_string));
-    
-    set_aircraft5_tlf_number2(conf.Get(aircraft5_config_section,
-                                  tlf_number2,
-                                  default_string));
-    
-    set_aircraft5_tlf_number3(conf.Get(aircraft5_config_section,
-                                  tlf_number3,
-                                  default_string));
-
-    set_aircraft5_rock_address(conf.Get(aircraft5_config_section,
-                                  rock_address,
-                                  default_string));
-
-    set_aircraft5_mav_id(conf.GetInteger(
-          aircraft5_config_section, mav_id, default_int));
+  /* [aircraft5] config section */ 
+  set_aircraft5_enabled(conf.GetBoolean(aircraft5_config_section, enabled,
+                                  default_enabled));
+  
+  set_aircraft5_tlf_number1(conf.Get(aircraft5_config_section,
+                                tlf_number1,
+                                default_string));
+  
+  set_aircraft5_tlf_number2(conf.Get(aircraft5_config_section,
+                                tlf_number2,
+                                default_string));
+  
+  set_aircraft5_tlf_number3(conf.Get(aircraft5_config_section,
+                                tlf_number3,
+                                default_string));
+  set_aircraft5_rock_address(conf.Get(aircraft5_config_section,
+                                rock_address,
+                                default_string));
+  set_aircraft5_mav_id(conf.GetInteger(
+        aircraft5_config_section, mav_id, default_int));
 
   return 0;
 }
 
-bool Config::get_debug_mode() const { return debug_mode; }
-
-void Config::set_debug_mode(bool debug) { debug_mode = debug; }
+/* Autopilot */
 
 std::string Config::get_autopilot_serial() const { return autopilot_serial; }
+void Config::set_autopilot_serial(const std::string& path) {autopilot_serial = path;}
+
+int Config::get_autopilot_serial_speed() const {return autopilot_serial_speed;}
+void Config::set_autopilot_serial_speed(int speed) {autopilot_serial_speed = speed;}
+
+/* RFD */
 
 std::string Config::get_rfd_serial() const { return rfd_serial; }
+void Config::set_rfd_serial(const std::string& path) {rfd_serial = path;}
 
-void Config::set_autopilot_serial(const std::string& path) {
-  autopilot_serial = path;
-}
+int Config::get_rfd_serial_speed() const {return rfd_serial_speed;}
+void Config::set_rfd_serial_speed(int speed) {rfd_serial_speed = speed;}
 
-int Config::get_autopilot_serial_speed() const {
-  return autopilot_serial_speed;
-}
-
-void Config::set_rfd_serial(const std::string& path) {
-  rfd_serial = path;
-}
-
-int Config::get_rfd_serial_speed() const {
-  return rfd_serial_speed;
-}
-
-void Config::set_autopilot_serial_speed(int speed) {
-  autopilot_serial_speed = speed;
-}
-
-void Config::set_rfd_serial_speed(int speed) {
-  rfd_serial_speed = speed;
-}
-
-bool Config::get_isbd_enabled() const { return isbd_enabled; }
-
-bool Config::get_autopilot_enabled() const { return autopilot_enabled; }
-
-bool Config::get_rfd_enabled() const { return rfd_enabled; }
-
-void Config::set_rfd_enabled(bool enabled) { rfd_enabled = enabled; }
-
-void Config::set_autopilot_enabled(bool enabled) { autopilot_enabled = enabled; }
-
-void Config::set_isbd_enabled(bool enabled) { isbd_enabled = enabled; }
-
-std::string Config::get_isbd_serial() const { return isbd_serial; }
-
-void Config::set_isbd_serial(const std::string& path) { isbd_serial = path; }
+/* ISBD */
 
 int Config::get_isbd_serial_speed() const { return isbd_serial_speed; }
-
 void Config::set_isbd_serial_speed(int speed) { isbd_serial_speed = speed; }
 
-bool Config::get_auto_detect_serials() const { return auto_detect_serials; }
+std::string Config::get_isbd_serial() const { return isbd_serial; }
+void Config::set_isbd_serial(const std::string& path) { isbd_serial = path; }
 
-void Config::set_auto_detect_serials(bool a) { auto_detect_serials = a; }
+/* GSM */
 
-bool Config::get_air_unit() const { return air_unit; }
+int Config::get_gsm_serial_speed() const { return gsm_serial_speed; }
+void Config::set_gsm_serial_speed(int speed) { gsm_serial_speed = speed; }
 
-void Config::set_air_unit(bool a) { air_unit = a; }
+std::string Config::get_gsm_serial() const { return gsm_serial; }
+void Config::set_gsm_serial(const std::string& path) { gsm_serial = path; }
 
-double Config::get_isbd_report_period() const { return isbd_report_period; }
-
-void Config::set_isbd_report_period(double period) { isbd_report_period = period; }
-
-bool Config::get_tcp_enabled() const { return tcp_enabled; }
-
-void Config::set_tcp_enabled(bool enabled) { tcp_enabled = enabled; }
-
-std::string Config::get_tcp_host() const { return tcp_host; }
-
-void Config::set_tcp_host(const std::string& host) { tcp_host = host; }
+/* TCP */
 
 int Config::get_tcp_port() const { return tcp_port; }
-
 void Config::set_tcp_port(int port) { tcp_port = port; }
 
-double Config::get_tcp_report_period() const { return tcp_report_period; }
+/* misc configuration properties */
 
-void Config::set_tcp_report_period(double period) { tcp_report_period = period; };
+bool Config::get_debug_mode() const { return debug;}
+void Config::set_debug_mode(bool deb) { debug = deb; }
 
- /* groundstation configuration properties */
+
+/* groundstation configuration properties */
 
 std::string Config::get_groundstation_tlf_number1() const { return groundstation_tlf_number1; }
 void Config::set_groundstation_tlf_number1(const std::string& number) { groundstation_tlf_number1 = number; }
