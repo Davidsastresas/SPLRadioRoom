4ec8e0e0e27603b029135dfd41b894b7b1a6c880
david sastre <davidsastresas@gmail.com>
2020-06-20 05:56:15 +0200

    deactivate isbd

--- srcair/MAVLinkHandlerAir.cc
+++ srcair/MAVLinkHandlerAir.cc
@@ -115,14 +115,14 @@ bool MAVLinkHandlerAir::init() {
   }
 
   // ----------------- ISBD ------------------
-  if (isbd_channel.init(config.get_isbd_serial(), config.get_isbd_serial_speed(), devices, config.get_groundstation_rock_address())) {
-    log(LOG_INFO, "ISBD channel initialized.");
-    isbd_initialized = true;
-  } else {
-    log(LOG_WARNING, "ISBD channel initialization failed.");
-    isbd_initialized = false;
-    // return false;
-  }
+  // if (isbd_channel.init(config.get_isbd_serial(), config.get_isbd_serial_speed(), devices, config.get_groundstation_rock_address())) {
+  //   log(LOG_INFO, "ISBD channel initialized.");
+  //   isbd_initialized = true;
+  // } else {
+  //   log(LOG_WARNING, "ISBD channel initialization failed.");
+  //   isbd_initialized = false;
+  //   // return false;
+  // }
 
   heartbeat_period = timelib::sec2ms(1);
   active_update_interval = timelib::sec2ms(0.1);
