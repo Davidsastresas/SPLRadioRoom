7daee3f0249173f434ac4ea31142d7939b3a39a3
david sastre <davidsastresas@gmail.com>
2020-06-21 20:25:04 +0200

    AirReport: dont update report if msgid > 250(GCS)

--- srcair/MAVReport.cc
+++ srcair/MAVReport.cc
@@ -48,12 +48,15 @@ MAVReport::MAVReport() : sysid(1), compid(1), mask(0) {}
  * message.
  */
 bool MAVReport::update(const mavlink_message_t& msg) {
+  if (msg.sysid > 250) {
+    return false;
+  }
   sysid = msg.sysid;
   switch (msg.msgid) {
     case MAVLINK_MSG_ID_HEARTBEAT:  // 0
       report.base_mode = mavlink_msg_heartbeat_get_base_mode(&msg);
       report.custom_mode = mavlink_msg_heartbeat_get_custom_mode(&msg);
-      compid = msg.compid;
+      // compid = msg.compid;
       mask |= mavlink_msg_mask_heartbeat;
       return true;
     case MAVLINK_MSG_ID_SYS_STATUS:  // 1
