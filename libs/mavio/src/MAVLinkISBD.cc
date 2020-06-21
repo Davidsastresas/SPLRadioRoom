3053f15c295ab95892942811e05607fb6103fc15
david sastre <davidsastresas@gmail.com>
2020-06-21 18:27:02 +0200

    SBD reduced bytes working

--- libs/mavio/src/MAVLinkISBD.cc
+++ libs/mavio/src/MAVLinkISBD.cc
@@ -49,6 +49,13 @@ bool MAVLinkISBD::get_ring_alert_flag(uint16_t& ra_flag) {
     mavio::log(LOG_INFO, "Ring alert received.");
   }
 
+  // mavio::log(LOG_INFO, "Message in MO buffer: %d", mo_flag);  
+  // mavio::log(LOG_INFO, "MO secuence number: %d", mo_msn);  
+  // mavio::log(LOG_INFO, "Message in MT buffer: %d", mt_flag);  
+  // mavio::log(LOG_INFO, "MT secuence in last: %d", mt_msn);  
+  // mavio::log(LOG_INFO, "Ring alert: %d", ra_flag);  
+  // mavio::log(LOG_INFO, "Messages waiting: %d", msg_waiting);  
+
   return err == ISBD_SUCCESS;
 }
 
@@ -157,15 +164,27 @@ bool MAVLinkISBD::message_available() {
 bool MAVLinkISBD::send_receive_message(const mavlink_message_t& mo_msg,
                                        mavlink_message_t& mt_msg,
                                        bool& received) {
-  uint8_t buf[ISBD_MAX_MT_MGS_SIZE];
-  size_t buf_size = sizeof(buf);
+  uint8_t buforiginal[ISBD_MAX_MT_MGS_SIZE];
+  size_t buf_size = sizeof(buforiginal);
   uint16_t len = 0;
 
   if (mo_msg.len != 0 && mo_msg.msgid != 0) {
-    len = mavlink_msg_to_send_buffer(buf, &mo_msg);
+    len = mavlink_msg_to_send_buffer(buforiginal, &mo_msg);
   }
 
   received = false;
+
+  uint8_t buf[ISBD_MAX_MT_MGS_SIZE];
+
+  buf[0] = buforiginal[4];
+  buf[1] = buforiginal[5];
+  buf[2] = buforiginal[7];
+
+  for (uint16_t i = 3; i < len - 7; i++) {
+    buf[i] = buforiginal[i + 7];
+  }
+
+  len = len - 7;
 
   int ret = isbd.sendReceiveSBDBinary(buf, len, buf, buf_size, remoteid);
 
@@ -185,8 +204,30 @@ bool MAVLinkISBD::send_receive_message(const mavlink_message_t& mo_msg,
   if (buf_size > 0) {
     mavlink_status_t mavlink_status;
 
+    uint8_t bufdecoded[ISBD_MAX_MT_MGS_SIZE];
+    uint8_t mavlink_size = buf_size - 5 - 3 - 2; // minus rockblock prefix, minus 3 headers (seq, id, msgid), minus crc
+
+    mavio::log(LOG_INFO, "mavlink lenght: %x", mavlink_size);
+
+    bufdecoded[0] = 0xFD;
+    bufdecoded[1] = mavlink_size;
+    bufdecoded[2] = 0x00;
+    bufdecoded[3] = 0x00;
+    bufdecoded[4] = buf[0 + 5]; // 0 init message plus 5 rockblock prefix
+    bufdecoded[5] = buf[1 + 5]; // 0 init message plus 5 rockblock prefix
+    bufdecoded[6] = 0x01;
+    bufdecoded[7] = buf[2 + 5]; // 0 init message plus 5 rockblock prefix
+    bufdecoded[8] = 0x00;
+    bufdecoded[9] = 0x00;
+
+    for (size_t i_dec = 8; i_dec < buf_size; i_dec++) {
+      bufdecoded[i_dec + 2] = buf[i_dec];
+    }
+
+    buf_size = buf_size + 7;
+
     for (size_t i = 0; i < buf_size; i++) {
-      if (mavlink_parse_char(MAVLINK_COMM_0, buf[i], &mt_msg,
+      if (mavlink_parse_char(MAVLINK_COMM_3, bufdecoded[i], &mt_msg,
                              &mavlink_status)) {
         received = true;
 
